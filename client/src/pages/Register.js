985630391d68375ba97e7263e00758bf3948a227
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-06-22 13:56:19 -0700

    update ereor

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -105,10 +105,10 @@ class Register extends Component {
 									/>
 								</div>
 							</div>
-							<div className="dropdown">
+							{/* <div className="dropdown">
 								<button className="btn btn-secondary dropdown-toggle my-3" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 									Dropdown button
-  								</button>
+  								</button> */}
 								{/* <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
 									<a className="dropdown-item" href="#">Mentor</a>
 									<a className="dropdown-item" href="#">Mentee</a>
@@ -130,6 +130,7 @@ class Register extends Component {
 					</div>
 				</div>
 			</div>
+		
 		)
 	}
 }
