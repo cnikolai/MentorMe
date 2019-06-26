8b58665798a1cdacdcd476c726c1cbecc4145186
Michael Putterflam <michael.putterflam@gmail.com>
2019-06-25 22:10:23 -0700

    getting data from indeed-scraper package

--- routes/indeedscraper.js
+++ routes/indeedscraper.js
@@ -14,13 +14,12 @@ const router = require("express").Router();
 // };
 
 router.get("/", function (req, res) {
-    const newObj = JSON.parse(req.query.queryOptions)
+    const newObj = JSON.parse(req.query.queryOptions);
     indeed.query(newObj).then(result => {
         res.json(result); // An array of Job objects
         console.log(result);
-
     });
-    console.log(req.query.queryOptions);
+    // console.log(req.query.queryOptions);
     // console.log(req.body);
     // console.log(req.params);
 
