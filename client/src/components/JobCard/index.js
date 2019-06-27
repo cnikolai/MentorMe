622712241fc2a83538538591c65f8a7d4b77bf57
Michael Putterflam <michael.putterflam@gmail.com>
2019-06-27 01:12:24 -0700

    You can enter a job and city to search. Links open in new tab.

--- client/src/components/JobCard/index.js
+++ client/src/components/JobCard/index.js
@@ -8,7 +8,7 @@ function JobCard(props) {
                 <h5 className="card-title">{props.title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted"></h6>
                 <p className="card-text">{props.summary}</p>
-                <a href={props.url} className="card-link">Job Link</a>
+                <a href={props.url} className="card-link" target="_blank">Job Link</a>
                 {/* <a href="#" className="card-link">Another link</a> */}
             </div>
         </div>
--- client/src/pages/Indeed.js
+++ client/src/pages/Indeed.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react'
 import JobCard from '../components/JobCard'
+import Wrapper from '../components/Wrapper'
 import './style.css'
 import axios from 'axios'
 
 class Indeed extends Component {
 
     state = {
+        jobBox: "",
+        cityBox: "",
         loading: false,
         jobListings: []
     };
 
     indeedSearch = () => {
 
-        this.setState({ loading: true });
+        this.setState({ loading: true, jobListings: [] });
 
         const queryOptions = {
             host: 'www.indeed.com',
-            query: 'Software',
-            city: 'Seattle, WA',
+            query: this.state.jobBox,
+            city: this.state.cityBox,
             radius: '25',
             level: 'entry_level',
             jobType: 'fulltime',
             maxAge: '7',
             sort: 'date',
-            limit: 100
+            limit: 25
         };
 
+        console.log(queryOptions);
         axios.get("/indeedscraper", {
             params: {
                 queryOptions: queryOptions
@@ -40,28 +44,39 @@ class Indeed extends Component {
         ).catch(err => console.log(err));
     }
 
-    componentDidCatch() {
-        this.indeedSearch();
+    handleJobChange = event => {
+        this.setState({ jobBox: event.target.value });
+    }
+
+    handleCityChange = event => {
+        this.setState({ cityBox: event.target.value });
     }
 
     render() {
 
         if (this.state.loading) {
             return (
-                <p>Loading Search Results</p>
+                <div className="loading-div">
+                    <p>Loading Search Results</p>
+                </div>
             );
         } else {
             return (
                 <div className="job-wrapper">
+                    Job: <input type="text" value={this.state.jobBox} onChange={this.handleJobChange}></input>
+                    City: <input type="text" placeholder="Los Angles, CA" value={this.state.cityBox} onChange={this.handleCityChange}></input>
                     <button onClick={this.indeedSearch}>Indeed Search</button>
 
-                    {this.state.jobListings.map((job, index) => (
-                        <JobCard key={index}
-                            title={job.title}
-                            summary={job.summary}
-                            url={job.url}
-                        />
-                    ))}
+                    <Wrapper>
+                        {this.state.jobListings.map((job, index) => (
+                            <JobCard key={index}
+                                title={job.title}
+                                summary={job.summary}
+                                url={job.url}
+                            />
+                        ))}
+                    </Wrapper>
+
                 </div>
             )
         }
