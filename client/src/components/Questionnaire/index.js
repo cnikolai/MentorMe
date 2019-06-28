13f5eec2e793145adb6b1d94980be283bf59e911
Jenn <jenn@Jenns-MacBook-Pro.local>
2019-06-27 19:14:06 -0700

    styling questionnaire radio buttons

--- client/src/components/Questionnaire/index.js
+++ client/src/components/Questionnaire/index.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import { Route, Link } from 'react-router-dom';
+import React from 'react';
 import "./style.css";
 
 function Questionnaire() {
@@ -11,47 +9,47 @@ function Questionnaire() {
 
             <p>1. I am interested in meeting up at least once a week.</p>
             <form>
-            <input type="radio" name="meetoften" value="1" /> 1
-            <input type="radio" name="meetoften" value="2" /> 2
-            <input type="radio" name="meetoften" value="3" /> 3
-            <input type="radio" name="meetoften" value="4" /> 4
-            <input type="radio" name="meetoften" value="5" /> 5
+            <input className="radiobtn" type="radio" name="meetoften" value="1" /> 1
+            <input className="radiobtn" type="radio" name="meetoften" value="2" /> 2
+            <input className="radiobtn" type="radio" name="meetoften" value="3" /> 3
+            <input className="radiobtn" type="radio" name="meetoften" value="4" /> 4
+            <input className="radiobtn" type="radio" name="meetoften" value="5" /> 5
             </form>
 
             <p>2. I want to meet in-person.</p>
             <form>
-            <input type="radio" name="inperson" value="1" /> 1
-            <input type="radio" name="inperson" value="2" /> 2
-            <input type="radio" name="inperson" value="3" /> 3
-            <input type="radio" name="inperson" value="4" /> 4
-            <input type="radio" name="inperson" value="5" /> 5
+            <input className="radiobtn" type="radio" name="meetoften" value="1" /> 1
+            <input className="radiobtn" type="radio" name="meetoften" value="2" /> 2
+            <input className="radiobtn" type="radio" name="meetoften" value="3" /> 3
+            <input className="radiobtn" type="radio" name="meetoften" value="4" /> 4
+            <input className="radiobtn" type="radio" name="meetoften" value="5" /> 5
             </form>
 
             <p>3. This industry is a hobby for me as opposed to my profession.</p>
             <form>
-            <input type="radio" name="hobby" value="1" /> 1
-            <input type="radio" name="hobby" value="2" /> 2
-            <input type="radio" name="hobby" value="3" /> 3
-            <input type="radio" name="hobby" value="4" /> 4
-            <input type="radio" name="hobby" value="5" /> 5
+            <input className="radiobtn" type="radio" name="meetoften" value="1" /> 1
+            <input className="radiobtn" type="radio" name="meetoften" value="2" /> 2
+            <input className="radiobtn" type="radio" name="meetoften" value="3" /> 3
+            <input className="radiobtn" type="radio" name="meetoften" value="4" /> 4
+            <input className="radiobtn" type="radio" name="meetoften" value="5" /> 5
             </form>
 
             <p>4. I am a beginner in this industry and am hoping to advance my skills quickly.</p>
             <form>
-            <input type="radio" name="beginner" value="1" /> 1
-            <input type="radio" name="beginner" value="2" /> 2
-            <input type="radio" name="beginner" value="3" /> 3
-            <input type="radio" name="beginner" value="4" /> 4
-            <input type="radio" name="beginner" value="5" /> 5
+            <input className="radiobtn" type="radio" name="meetoften" value="1" /> 1
+            <input className="radiobtn" type="radio" name="meetoften" value="2" /> 2
+            <input className="radiobtn" type="radio" name="meetoften" value="3" /> 3
+            <input className="radiobtn" type="radio" name="meetoften" value="4" /> 4
+            <input className="radiobtn" type="radio" name="meetoften" value="5" /> 5
             </form>
 
             <p>5. I would like structured mentor meetings with a schedule or agenda.</p>
             <form>
-            <input type="radio" name="structured" value="1" /> 1
-            <input type="radio" name="structured" value="2" /> 2
-            <input type="radio" name="structured" value="3" /> 3
-            <input type="radio" name="structured" value="4" /> 4
-            <input type="radio" name="structured" value="5" /> 5
+            <input className="radiobtn" type="radio" name="meetoften" value="1" /> 1
+            <input className="radiobtn" type="radio" name="meetoften" value="2" /> 2
+            <input className="radiobtn" type="radio" name="meetoften" value="3" /> 3
+            <input className="radiobtn" type="radio" name="meetoften" value="4" /> 4
+            <input className="radiobtn" type="radio" name="meetoften" value="5" /> 5
             </form>
 
             <button id="done" type="submit" value="text">Done</button>
