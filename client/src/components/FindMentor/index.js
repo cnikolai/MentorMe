c4a8ba5357cbff122335ef8f0c90b11cf77378d1
Jenn <jenn@Jenns-MBP.lan>
2019-07-06 11:47:59 -0700

    centered card and fixed overflow

--- client/src/components/FindMentor/index.js
+++ client/src/components/FindMentor/index.js
@@ -5,23 +5,25 @@ import "./style.css";
 
 class FindMentor extends Component {
 
-
     render() {
         return (
-            <div className="card text-center">
-                <img src={this.props.src} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.mentorname}</h5>
-                    <h6 className="card-title">Profession: {this.props.profession}</h6>
-                    <p className="card-text">{this.props.description}</p>
-                    <a href="#" id="connect" className="btn btn-primary">Connect</a>
-                    <a href="#" id="pass" className="btn btn-danger">Pass</a>
-                </div>
+            <div className="cardcontainer">
+                <h1 className="text-center">Make New Connections</h1>
+                    <div className="card text-center mx-auto">
+                        <img src={this.props.src} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{this.props.mentorname}</h5>
+                            <h6 className="card-title">Profession: {this.props.profession}</h6>
+                            <p className="card-text">{this.props.description}</p>
+                            <a href="#" id="connect" className="btn btn-primary">Connect</a>
+                            <a href="#" id="pass" className="btn btn-danger">Pass</a>
+                        </div>
+                    </div>
             </div>
         );
     };
 }
 
 
-        
+
 export default FindMentor;
