369a7de78e290e033306460930b7dad4ea04d695
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-07-06 13:47:34 -0700

    Dynamic populate for connect page added

--- client/src/components/FindMentor/index.js
+++ client/src/components/FindMentor/index.js
@@ -3,36 +3,53 @@ import React, { Component } from 'react';
 //import { Route, Link } from 'react-router-dom';
 import "./style.css";
 import Axios from 'axios';
-
-Axios.get("/users/mentors").then(response => {
-    console.log(response.data);
-   
-        this.setState(
-            {name: response.data.username},
-            {img: response.data.img},
-            {profession: response.data.profession},
-            {description: response.data.description}
-        )
-    
-});
 class FindMentor extends Component {
+    state = {
+        result: []
+    }
+
+    mentors = () => {
+        Axios.get("/users/mentors").then(response => {
+            console.log(response.data);
+                this.setState({
+                    result: response.data
+                })
+            
+        });
+    }
+
+    componentDidMount() {
+        this.mentors();
+    }
+
+    connectMentor = () => {
+        
+        console.log('Connected with Mentor.');
+    }
+    
+    passMentor = () => {
+       
+        console.log('Mentor Passed.');
+    }
 
     render() {
         return (
 
             <div className="cardcontainer">
                 <h1 className="text-center">Make New Connections</h1>
-                    <div className="card text-center mx-auto">
-                        <img src={this.props.src} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{this.props.mentorname}</h5>
-                            <h6 className="card-title">Profession: {this.props.profession}</h6>
-                            <p className="card-text">{this.props.description}</p>
-                            <a href="#" id="connect" className="btn btn-primary">Connect</a>
-                            <a href="#" id="pass" className="btn btn-danger">Pass</a>
+                    {/* loop through array and create card for obj */}
+                    {this.state.result.map(mentor => (
+                        <div className="card text-center mx-auto" key={mentor._id}>
+                            <img src={mentor.img} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{mentor.name}</h5>
+                                <h6 className="card-title">Profession: {mentor.profession}</h6>
+                                <p className="card-text">{mentor.description}</p>
+                                <a href="#" id="connect" className="btn btn-primary" onClick={this.connectMentor}>Connect</a>
+                                <a href="#" id="pass" className="btn btn-danger" onClick={this.passMentor}>Pass</a>
+                            </div>
                         </div>
-                    </div>
-
+                    ))}
             </div>
         );
     };
