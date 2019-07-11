166f61b7b41f1713d021fbf1cec06726a8079a1e
Michael Putterflam <michael.putterflam@gmail.com>
2019-07-10 18:31:07 -0700

    added error banner for error

--- client/src/components/Matches/index.js
+++ client/src/components/Matches/index.js
@@ -46,19 +46,11 @@ class Matches extends Component {
             <div className="container-fluid text-center">
                 <h1 className="text-center">Your Connections</h1>
 
-                {/* <div className="row"> */}
+                {this.state.errorHappened ? <ErrorBanner>Error. Something went wrong. Could not delete mentor</ErrorBanner> : null}
+
                 <Wrapper>
                     {this.state.mentors.map(mentor => (
-                        // <div className="card" key={mentor._id}>
-                        //     <img className="card-img-top" src={mentor.img} alt="Card image cap" />
-                        //     <div className="card-body">
-                        //         <h5 className="card-title">{mentor.username}</h5>
-                        //         <h6 className="card-title">Profession: {mentor.profession}</h6>
-                        //         <h6 className="card-title">Email: {mentor.email}</h6>
-                        //         {/* <p className="card-text">Avid pursuer of clues and interested in broadening horizons</p> */}
-                        //         <a href="#" className="btn btn-primary send-mes">Send a Message</a>
-                        //     </div>
-                        // </div>
+
                         <SavedMentorCard key={mentor._id}
                             img={mentor.img}
                             username={mentor.username}
@@ -69,47 +61,7 @@ class Matches extends Component {
                         />
                     ))}
                 </Wrapper>
-                {/* <div className="card col">
-                        <img className="card-img-top" src="https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Pika Chu</h5>
-                            <h6 className="card-title">Profession: Detective</h6>
-                            <p className="card-text">Avid pursuer of clues and interested in broadening horizons</p>
-                            <a href="#" className="btn btn-primary send-mes">Send a Message</a>
-                        </div>
-                    </div> */}
 
-                {/* <div className="card col">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Name</h5>
-                            <h6 className="card-title">Profession</h6>
-                            <p className="card-text">Brief information about interests and profession.</p>
-                            <a href="#" className="btn btn-primary send-mes">Send a Message</a>
-                        </div>
-                    </div>
-
-                    <div className="card col">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Name</h5>
-                            <h6 className="card-title">Profession</h6>
-                            <p className="card-text">Brief information about interests and profession.</p>
-                            <a href="#" className="btn btn-primary send-mes">Send a Message</a>
-                        </div>
-                    </div>
-
-                    <div className="card col">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Name</h5>
-                            <h6 className="card-title">Profession</h6>
-                            <p className="card-text">Brief information about interests and profession.</p>
-                            <a href="#" className="btn btn-primary send-mes">Send a Message</a>
-                        </div>
-                    </div> */}
-
-                {/* </div> */}
             </div>
         );
     };
